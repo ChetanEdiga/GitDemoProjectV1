d0ec92aaeefa834b5ab1fd4e714bd7bb677d9ed9
ChetanEdiga <chetangoud3@gmail.com>
2022-06-21 20:47:43 +0530

    Created class Test2 as second commit

---
+++ src/Test2.java
@@ -0,0 +1,17 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String  x = "hungry";
+		
+		if(x.contains("hungry")) {
+
+		System.out.println("I should eat some thing");	
+
+	}else {
+		System.out.println("Feels heavy stomach");
+	
+	}
+	}
+
+}
